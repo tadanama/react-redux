e3df00d3687b6f08407886ad0e10b75a300472b0
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-02 21:46:45 +0800

    Add all of the counter elements
    - Button to increment, decrement and reset the counter
    - Input field to increment counter by specific amount

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -6,7 +6,25 @@ function Counter() {
 	// Retrieve the state from redux
 	const count = useSelector((state) => state.counter.count);
 
-	
+	// Triggers an action
+	const dispatch = useDispatch();
+
+	return (
+		<section>
+			<p>{count}</p>
+			<button>+</button>
+			<button>-</button>
+
+			<br />
+
+			<input type="text" />
+
+			<br />
+
+			<button>Increment by amount</button>
+			<button>Reset</button>
+		</section>
+	);
 }
 
 export default Counter;
