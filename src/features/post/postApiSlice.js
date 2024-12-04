e5ead143d3b2315646aad89c5d5bd4ea6030e5b8
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-04 20:53:15 +0800

    Create api post slice that use RTK query

---
+++ src/features/post/postApiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Create api post slice that use RTK query
+export const postApiSlice = createApi({
+    reducerPath: "post",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com"}),
+    endpoints: (builder) => {
+        return {
+            getPosts: builder.query({
+                // Fetch posts at "https://jsonplaceholder.typicode.com/posts"
+                query: () => "/posts"
+            })
+        }
+    }
+})
