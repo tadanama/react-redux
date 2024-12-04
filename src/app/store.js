80231044f5605e2b7f5b33ffccf55241040a72bc
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-04 20:54:31 +0800

    Include postApiSlice in redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import { postApiSlice } from "../features/post/postApiSlice";
 
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
+		[postApiSlice.reducerPath]: postApiSlice.reducer,
 	},
 });
