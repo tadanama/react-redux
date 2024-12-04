305d79b9b04b47e195ad7328bcbd01802fb010dc
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-04 21:15:46 +0800

    Render the posts
    - Get all the posts using the useGetPostQuery custom hook
    - Render all of them as an unordered list
    - Include the Posts component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Counter from "./features/counter/Counter.jsx";
+import Posts from "./features/post/Posts.jsx";
 
 function App() {
 	return (
 		<>
 			<Counter />
+			<Posts />
 		</>
 	);
 }
--- src/features/post/Posts.jsx
+++ src/features/post/Posts.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useGetPostsQuery } from "./postApiSlice";
 
 function Posts() {
-	return <div></div>;
+	// Get posts
+	// Can be done by calling the postsApiSlice custom hook
+	const { data: posts } = useGetPostsQuery();
+	return (
+		<div>
+			<ul>
+				{posts?.map((post) => {
+					return <li key={post.id}>{post.title}</li>;
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default Posts;
