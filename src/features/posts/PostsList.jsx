19dbab031cc200b3af45263ba4fff75f7de66ec8
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-06 17:28:44 +0800

    Display all of the posts in the redux store
    - Retrieve the post state (an array of objects)
    - Map through each element to display the title and content

---
+++ src/features/posts/PostsList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { selectAllPosts } from "./postsSlice";
+import { render } from "react-dom";
+
+function PostsList() {
+    // Retrieve the state of the posts
+    // Will return an array of posts because an array of posts is its initial state
+    const posts = useSelector(selectAllPosts);
+
+    // Map through all of the elements to display title and content
+    const renderedPosts = posts.map(post => (
+        <article key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+        </article>
+    ))
+
+    return <>
+    <h1>Posts</h1>
+    {renderedPosts}
+    </>
+
+}
+
+export default PostsList;
