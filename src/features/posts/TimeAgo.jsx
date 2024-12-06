0eaec3bbfc334eea0f83c60de6bf917f0bd5f60f
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-06 22:17:23 +0800

    Create time ago component
    - Display the time since the post was posted in a readable way

---
+++ src/features/posts/TimeAgo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { formatDistanceToNow } from "date-fns";
+
+function TimeAgo({ timestamp }) {
+	return (
+		<>
+			<span> Posted {formatDistanceToNow(timestamp)} ago</span>
+		</>
+	);
+}
+
+export default TimeAgo;
