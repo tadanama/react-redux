973343e59d4e39fe0ba5cba3aa11801313701f18
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-06 21:31:02 +0800

    Render form to add new post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
 	return (
 		<>
+			<AddPostForm />
 			<PostsList />
 		</>
 	);
