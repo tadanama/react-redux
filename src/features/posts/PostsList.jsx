d71cd58e6680c37af3697d41501f5afaa6f4277d
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-06 21:40:19 +0800

    Display list of posts from the redux store

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import { selectAllPosts } from "./postsSlice";
-import { render } from "react-dom";
+import { selectAllUsers } from "../users/usersSlice";
+import PostAuthor from "./PostAuthor";
 
 function PostsList() {
-    // Retrieve the state of the posts
-    // Will return an array of posts because an array of posts is its initial state
-    const posts = useSelector(selectAllPosts);
+	// Retrieve the state of the posts
+	// Will return an array of posts because an array of posts is its initial state
+	const posts = useSelector(selectAllPosts);
 
-    // Map through all of the elements to display title and content
-    const renderedPosts = posts.map(post => (
-        <article key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-        </article>
-    ))
-
-    return <>
-    <h1>Posts</h1>
-    {renderedPosts}
-    </>
-
+	// Map through all of the elements to display title, content and author
+	const renderedPosts = posts.map((post) => (
+		<article key={post.id}>
+			<h2>{post.title}</h2>
+			<p>{post.content}</p>
+            <PostAuthor userId={post.userId}/>
+		</article>
+	));
+	return (
+		<>
+			<h1>Posts</h1>
+			{renderedPosts}
+		</>
+	);
 }
 
 export default PostsList;
