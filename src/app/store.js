a90dc6e39a867efb7a69d6f366312f7fad3cda1c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-06 21:32:00 +0800

    Add user reducer to the redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/posts/postsSlice";
+import userReducer from "../features/users/usersSlice";
 
 export const store = configureStore({
 	reducer: {
 		// The key below (post in this case) is the name that we will use to access the state later
 		// state.<keyBelow> (state.post in this case)
 		post: postReducer,
+		user: userReducer,
 	},
 });
