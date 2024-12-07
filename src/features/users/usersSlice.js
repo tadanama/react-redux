7637962a4cf6e8c7c4f0c8f748964a76772da9aa
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-07 14:46:15 +0800

    Fetch user state from jsonplaceholder api
    - Change initial state to an empty array
    - Define a thunk to synchronously fetch the users
    - Spread the array of fetched users and push it to the initial state or overwrite the state completely by just returning the payload

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,17 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 // Declaring users initial state
-const initialState = [
-	{ id: 1, name: "Syakir" },
-	{ id: 2, name: "Haziq" },
-	{ id: 3, name: "Tadanama" },
-];
+const initialState = [];
+
+const USER_URL = "https://jsonplaceholder.typicode.com/users"
+
+export const fetchUsers = createAsyncThunk("user/fetchUsers", async () => {
+	try {
+		const result = await axios.get(USER_URL);
+
+		// Return the data (an array of user objects) below as the payload to the reducer
+		return result.data;
+	} catch (error) {
+		console.log(error);
+		return error.message;
+	}
+})
 
 // Defining users slice
 const usersSlice = createSlice({
 	name: "user",
 	initialState,
 	reducers: {},
+	extraReducers: (builder) => {
+		builder.addCase(fetchUsers.pending, (state) => {
+			console.log("fetching users")
+		})
+		.addCase(fetchUsers.fulfilled, (state, action) => {
+			// Spread the action payload and push to the state
+			state.push(...action.payload);
+
+			// Overwrite the state completely with the action payload
+			// return action.payload;
+		})
+	}
 });
 
 // Exporting users action creators
