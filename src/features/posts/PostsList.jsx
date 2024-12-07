c233e6942ee08001a82c1e43254ea21ad48109f2
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-07 15:04:05 +0800

    Render the fetched posts
    - Dispatch an action to get the posts on the first render
    - Show different content based on the posts status state
    - When the action is fulfilled, filter the fetched posts so there are no duplicate posts
    - After that map through all the posts, so that each posts will have its own posts excerpt and render it as the content

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,37 +1,72 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { selectAllPosts } from "./postsSlice";
+import {
+	selectAllPosts,
+	getPostError,
+	getPostStatus,
+	fetchposts,
+} from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import PostsExcerpt from "./PostsExcerpt";
 
 function PostsList() {
 	// Retrieve the state of the posts
 	// Will return an array of posts because an array of posts is its initial state
 	const posts = useSelector(selectAllPosts);
 
-	// Sort the post
-	// Recent post should go to the top
-	const orderedPosts = posts
-		.slice()
-		.sort((a, b) => b.date.localeCompare(a.date));
+	// Get the status when fetching posts from redux store
+	const postsStatus = useSelector(getPostStatus);
 
-	// Map through all of the elements to display title, content, author, how long ago it was posted and the like and dislike button
-	const renderedPosts = orderedPosts.map((post) => (
-		<article key={post.id}>
-			<h2>{post.title}</h2>
-			<p>{post.content}</p>
-			<PostAuthor userId={post.userId} />
-			<TimeAgo timestamp={post.date} />
-            <ReactionButtons post={post}/>
-		</article>
-	));
+	// Get the error when fetching posts from redux store
+	const postsError = useSelector(getPostError);
+
+	const dispatch = useDispatch();
+
+	// Run useEffect on first render and if postStatus changes
+	// dispatch is provided as the dependency to avoid eslint warnings
+	// Fetch posts if status is idle
+	useEffect(() => {
+		if (postsStatus === "idle") {
+			dispatch(fetchposts());
+		}
+	}, [postsStatus, dispatch]);
+
+	// Display differnt content depending on posts status
+	let content;
+
+	if (postsStatus === "pending") {
+		content = <h2>Loading...</h2>;
+	} else if (postsStatus === "succeeded") {
+		// Sort the post
+		// Recent post should go to the top
+
+		const orderedPosts = posts
+			.slice()
+			.sort((a, b) => b.date.localeCompare(a.date));
+
+		// Remove duplicate posts
+		// Refer to the comment section on Dave Gray's redux video for more info
+		const preContent = orderedPosts.filter((post, index) => {
+			if (index !== orderedPosts.length - 1) {
+				return post.id !== orderedPosts[index + 1].id;
+			}
+			return post;
+		});
+
+		// Map through all of the elements to display title, content, author, how long ago it was posted and the like and dislike button from post excerpt
+		content = preContent.map((post) => (
+			<PostsExcerpt key={post.id} post={post} />
+		));
+
+	} else if (postsStatus === "failed") {
+		content = <p>{postsError}</p>;
+	}
+
 	return (
 		<>
 			<h1>Posts</h1>
-			{renderedPosts}
+			{content}
 		</>
 	);
 }
