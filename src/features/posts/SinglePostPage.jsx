629fc45756c98e0026e4fafd61e576a775473f41
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-07 21:27:34 +0800

    Retrieve postId with useParams hook
    - Convert the id to a number so that selectPostById selector can strictly compare the post id and find the post

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -5,13 +5,15 @@ import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
+import { useParams } from "react-router-dom";
 
 function SinglePostPage() {
 	// Retrieve specific postId using useParams
+	const { postId } = useParams();
 
-	const post = useSelector((state) => selectPostbyId(state, postId));
+	const post = useSelector((state) => selectPostbyId(state, Number(postId)));
 
-    // If no post found render heading below
+	// If no post found render heading below
 	if (!post) {
 		return <h2>Post not found</h2>;
 	}
