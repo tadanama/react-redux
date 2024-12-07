3eda19ff4908dfb14857de176caf98c1ccc3a576
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-07 21:05:11 +0800

    Set the nested routes in App component
    - Layout component determines the layout of the page (header, footer)
    - Root / route should always render PostsList
    - /post route should always render AddPostForm
    - /post/<postId> should render the SinglePostPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./components/Layout";
 
 function App() {
 	return (
 		<>
-			<AddPostForm />
-			<PostsList />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<PostsList />} />
+
+					<Route path="/post">
+						<Route index element={<AddPostForm />} />
+						<Route path=":postId" element={<SinglePostPage />} />
+					</Route>
+				</Route>
+			</Routes>
+
 		</>
 	);
 }
