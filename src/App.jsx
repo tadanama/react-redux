fa2fd91423832dd6f50f718731c3a976913e518c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-08 15:25:57 +0800

    Set route to render the EditPostForm

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import Layout from "./components/Layout";
+import EditPostForm from "./features/posts/EditPostForm";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 					<Route path="/post">
 						<Route index element={<AddPostForm />} />
 						<Route path=":postId" element={<SinglePostPage />} />
+						<Route path="edit/:postId" element={<EditPostForm />} />
 					</Route>
 				</Route>
 			</Routes>
