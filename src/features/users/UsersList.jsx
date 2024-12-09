ed4c6f5c59a3c06f2b4d5724e2db2c9e6829c222
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-09 11:18:48 +0800

    Add link to the UserPage

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import { selectAllUsers } from "./usersSlice";
+import { Link } from "react-router-dom";
 
 function UsersList() {
 	const users = useSelector(selectAllUsers);
 
+	const renderedUsers = users.map((user) => (
+		<li key={user.id}>
+			<Link to={`/user/${user.id}`}>{user.name}</Link>
+		</li>
+	));
+
 	return (
 		<>
-			<ol>
-				{users.map((user) => (
-					<li>{user.name}</li>
-				))}
-			</ol>
+			<h2>Users</h2>
+            {renderedUsers}
 		</>
 	);
 }
